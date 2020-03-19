dc35021f89abb452db1fac1d07f3f2129253df77
marissaverdonck <marissa.verdonck@hva.nl>
2020-03-19 13:18:08 +0100

    bodyparser

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-  // const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 3000;
 const host = '0.0.0.0';
+const bodyParser = require('body-parser');
 
 
 
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-//   extended: true
-// }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,13 +23,12 @@ app.get('/', (req, res) => {
 
 app.get('/design/:shirtColor/:text/:shirtImage', (req, res) => {
   res.render('design')
-    //   console.log(req.params.shirtColor)
 })
 
-// app.post('/', (req, res) => {
-//   console.log(req.body)
-//   res.redirect('/design/' + req.body.shirtColor + '/' + req.body.text + '/' + req.body.shirtImage)
-// })
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.redirect('/design/' + req.body.shirtColor + '/' + req.body.text + '/' + req.body.shirtImage)
+})
 
 
 
