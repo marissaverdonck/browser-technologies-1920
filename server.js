1e9f7fba1d241db704d76988227554aaf12be44a
marissaverdonck <marissa.verdonck@hva.nl>
2020-03-19 12:13:23 +0100

    env

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
+  // const bodyParser = require('body-parser');
 
-const port = process.env.port || 3000
+const PORT = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-  extended: true
-}));
+
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+//   extended: true
+// }));
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
-app.set('public', 'public');
+app.set('views', 'views');
 
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.get('/design/:shirtColor/:text/:shirtImage', (req, res) => {
   res.render('design')
-  console.log(req.params.shirtColor)
+    //   console.log(req.params.shirtColor)
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body)
-  res.redirect('/design/' + req.body.shirtColor + '/' + req.body.text + '/' + req.body.shirtImage)
+// app.post('/', (req, res) => {
+//   console.log(req.body)
+//   res.redirect('/design/' + req.body.shirtColor + '/' + req.body.text + '/' + req.body.shirtImage)
+// })
+
+
+
+app.listen(PORT, host, function() {
+  console.log(`Example app listening on port ${port}!`);
 });
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
